626a37358102fb224da831bc439d53816e8bf7f3
shawonislam1109 <shawonislam1109@gmail.com>
2022-10-25 13:16:46 +0600

    all intstall even firebase  flowbite react-router-dom react

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCddNgQyG8o0dRSqESezlUM39GmdEUe8nM",
+    authDomain: "learning-platform-4eb1d.firebaseapp.com",
+    projectId: "learning-platform-4eb1d",
+    storageBucket: "learning-platform-4eb1d.appspot.com",
+    messagingSenderId: "213456056181",
+    appId: "1:213456056181:web:d19d85f388416c17a5ed3c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app; 
